b8015179dde257127a5a04f819d03c9b323e4edb
ElijahA1 <elijah.abourezk@outlook.com>
2025-08-08 07:41:42 -0700

    Add project files.

---
+++ Assignment 5.3.2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Assignment_5._3._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int steps = 4;
+            Console.WriteLine(stepCombinations(steps));
+        }
+        static int stepCombinations(int steps)
+        {
+            if (steps == 0) { return 1; }
+            if (steps < 0) { return 0; }
+
+            return stepCombinations(steps - 1) + stepCombinations(steps - 2);
+        }
+    }
+}
